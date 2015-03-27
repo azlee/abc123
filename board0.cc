c477c282b721e0106dcf7628842a0697261055fa
Amber Lee <amber_Lee1993@HOtmail.com>
2015-03-27 00:40:47 -0400

    Update and rename grid.h to board0.cc

---
+++ board0.cc
@@ -0,0 +1,34 @@
+#include <string>
+#include "board0.h"
+#include <iostream>
+#include <fstream>
+
+void Board0::updateBoard() { // fill in empty squares
+	int numEmptySquares = 0; // recorded for each column
+	Square *temp;
+	for (int j =0; j < 10; j++ {
+		for (int i = 9; i != -1; i--) {
+			temp = mainBoard[i][j];
+			if (temp->getType == '-') {
+				numEmptySquares++;
+				if (incr - 1 == numEmptySquares) { // then the rest of the top cells in that column must be empty
+				// call some function to generate random squares for these cells.
+					//
+					//
+					// Implement..
+					//
+					//
+				}
+				else { // recurse up til we find a non empty square and then set temp to that square's properties
+					 // and delete the properties of that non empty square
+					for (int incr = 1; incr - i >= 0; incr++) { 
+						if (mainBoard[i - incr][j]->getType() != '-') { // square is not empty
+							temp->setType(mainBoard[i-incr][j]->getType());
+							mainBoard[i - incr][j]->setType('-');
+						}
+					}
+				}
+			}
+		}
+	}
+}
