47803724e20f05b7ed236624856c77462561abd4
Amber Lee <amber_Lee1993@HOtmail.com>
2015-03-28 17:31:13 -0400

    Update board0.h

--- board0.h
+++ board0.h
@@ -1,12 +1,18 @@
 #ifndef __BOARD0_H__
 #define __BOARD0_H__
 #include <iostream>
+#include <string>
 #include "Board.h"
+
 class Board0 : public{
-public:
-    Board0();
-    void fillBoard(int l);
-    ~Board0();
+    private:
+        int numGenerated = 0;
+        int leftoverLen;
+        std::string leftover;
+    public:
+        Board0();
+        void fillBoard(int l);
+        ~Board0();
 };
 
 #endif
